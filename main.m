3757f29698f7b22dc48bdf4b8c430affcdc0a2a4
Ai Xin <scicomath@outlook.com>
2016-09-25 21:44:33 +0800

    更新main脚本

--- main.m
+++ main.m
@@ -4,22 +4,10 @@ imgName = ls(fullfile('qukuaitu3',int2str(M),'*.jpg'));
 N = size(imgName,1)
 i = randi(N)
 img = imread(fullfile('qukuaitu3',int2str(M),imgName(i,:)));
-figure
-imshow(img)
-level = graythresh(img);
-BW = im2bw(img,level);
-BW = ~BW;
-figure
-imshow(BW)
-
-se = strel('disk',1);
-se2 = strel('disk',2);
-BW2 = imopen(BW,se);
-figure, imshow(BW2)
-BW3 = imclose(BW2,se2);
-figure, imshow(BW3)
-
-CC = bwconncomp(BW3);
-L = labelmatrix(CC);
-% L = bwlabel(BW3,4);
+% 图像预处理
+[ BW,L ] = imgPreprocessFun( img,1 );
 figure, imshow(label2rgb(L));
+% 上下分区区域
+[ k,upImg,downImg ] = splitImgFun( BW,0 );
+% 寻找度号
+[duIndex] = findDuFun( upImg );
