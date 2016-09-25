0e6f728b93d3ea0abee41a6e3505681d24f63ced
Ai Xin <scicomath@outlook.com>
2016-09-25 19:45:20 +0800

    修改分割函数,返回分割后上下两个图像

--- splitImgFun.m
+++ splitImgFun.m
@@ -1,12 +1,16 @@
-function [ k ] = splitImgFun( img,verbose )
+function [ k,upImg,downImg ] = splitImgFun( img,verbose )
 %splitImgFun 将区块分为两行, 返回分割线的行数
 %   
 [i,~] = find(img==1);
 [~,c] = kmeans(i,2);
 k = round((c(1,:)+c(2,:))/2);
+upImg = img(1:k,:);
+downImg = img(k:end,:);
 if verbose
     img(k,:) = 1;
     figure;imshow(img)
+    figure;imshow(upImg)
+    figure;imshow(downImg)
 end
 end
 
